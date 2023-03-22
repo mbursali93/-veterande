4166b217c8939c3649bec39cf0a8b042f36e3d82
Mustafa Bursali <m.bursali16@gmail.com>
2023-03-23 00:43:14 +0300

    cors for user service

--- user-service/server.js
+++ user-service/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv").config()
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
 const authRouter = require("./src/routes/auth-route")
 const ownerRouter = require("./src/routes/owner-route")
@@ -12,6 +13,10 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(cors({
+    credentials: true,
+    origin: [process.env.USER_LINK, process.env.FRONT_LINK]
+}))
 
 //ROUTES
 
